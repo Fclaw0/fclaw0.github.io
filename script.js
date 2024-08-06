0beef7c4a4b05b5142843d3ecfdf522f9b791fa1
Fclaw0 <fclaw@mun.ca>
2024-08-06 19:41:14 -0230

    Added back to top button, bug fixes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// TYPEWRITE TEXT
+
 const typedText = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
 
@@ -42,3 +44,24 @@ const type = () => {
 document.addEventListener("DOMContentLoaded", () => {
   type();
 });
+
+// BACK TO TOP BUTTON
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
